a1a6c3b509a73423aca064f166d1c4086d1eee71
Mikhail Dubov <mdubov@gmail.com>
2014-06-10 17:24:19 +0300

    Added columns and diagonals

--- lib/board.rb
+++ lib/board.rb
@@ -2,16 +2,25 @@ require_relative "./cell.rb"
 
 class Board
 
-  attr_reader :cells
+  attr_reader :cells, :size
 
-  BOARD_SIZE = 9
+  BOARD_SIZE = 3
 
   def initialize
-    @cells = Array.new(BOARD_SIZE, Cell.new)
+    @size = BOARD_SIZE
+    @cells = Array.new(size ** 2, Cell.new)
   end
 
   def rows
-    cells.each_slice(3).to_a
+    cells.each_slice(size).to_a
+  end
+
+  def columns
+    rows.transpose
+  end
+
+  def diagonals
+  [] << (0...@size).map { |i| rows[i][i] } << (0...@size).map { |i| rows.reverse[i][i] }
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,5 +15,12 @@ describe Board do
     it 'rows' do
       expect(board.rows.first).to eq board.cells[0..2]
     end
+    it 'columns' do
+      expect(board.columns.first).to eq [] << board.cells[0] << board.cells[3] << board.cells[6]
+    end
+    it 'diagonals' do
+      expect(board.diagonals.first).to eq [] << board.cells[0] << board.cells[4] << board.cells[8]
+      expect(board.diagonals.last).to eq [] << board.cells[2] << board.cells[4] << board.cells[6]
+    end
   end
 end
