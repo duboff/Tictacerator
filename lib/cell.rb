9ef2b1e2590feb061445395b5c69ea309bbf67a7
Mikhail Dubov <mdubov@gmail.com>
2014-06-10 16:43:35 +0300

    Added value functionality to cell

---
+++ lib/cell.rb
@@ -0,0 +1,8 @@
+class Cell
+
+  attr_accessor :value
+
+  def initialize(value = nil)
+    @value = value
+  end
+end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,1 +1,15 @@
 require 'cell'
+
+describe Cell do
+  let(:cell) {Cell.new}
+
+  context 'value' do
+    it 'is defined' do
+      expect(cell).to respond_to(:value)
+    end
+
+    it 'is nil if not specified' do
+      expect(cell.value).to eq nil
+    end
+  end
+end
