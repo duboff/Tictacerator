f2b60f8d5ac3399416902bac7156b841d86c8e64
Mikhail Dubov <mdubov@gmail.com>
2014-06-10 16:55:11 +0300

    basic cell checks added

--- lib/cell.rb
+++ lib/cell.rb
@@ -5,4 +5,16 @@ class Cell
   def initialize(value = nil)
     @value = value
   end
+
+  def x?
+    self.value == 1
+  end
+
+  def o?
+    self.value == 0
+  end
+
+  def empty?
+    self.value.nil?
+  end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -11,5 +11,28 @@ describe Cell do
     it 'is nil if not specified' do
       expect(cell.value).to eq nil
     end
+
+    it 'can be set to 1 or 0' do
+      cell.value = 1
+      expect(cell.value).to eq 1
+      cell.value = 0
+      expect(cell.value).to eq 0
+    end
+  end
+
+  context 'checks' do
+    
+    it 'if it is empty' do
+      expect(cell).to be_empty
+    end
+
+    it 'if value is x' do
+      cell.value = 1
+      expect(cell).to be_x
+    end
+    it 'if value is y' do
+      cell.value = 0
+      expect(cell).to be_o
+    end
   end
 end
