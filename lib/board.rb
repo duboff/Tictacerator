0066ac018290e897172676330e1b2965ce3169dd
Mikhail Dubov <mdubov@gmail.com>
2014-06-10 17:07:56 +0300

    Added rows

--- lib/board.rb
+++ lib/board.rb
@@ -4,8 +4,14 @@ class Board
 
   attr_reader :cells
 
+  BOARD_SIZE = 9
+
   def initialize
-    @cells = Array.new(9, Cell.new)
+    @cells = Array.new(BOARD_SIZE, Cell.new)
+  end
+
+  def rows
+    cells.each_slice(3).to_a
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,5 +7,13 @@ describe Board do
     it 'has 9 cells' do
       expect(board.cells.count).to eq 9
     end
+    it 'has only empty cells' do
+      expect(board.cells.map(&:value).uniq).to eq [nil]
+    end
+  end
+  context 'groups' do
+    it 'rows' do
+      expect(board.rows.first).to eq board.cells[0..2]
+    end
   end
 end
